0357b2d08b6aac2d33f181894b6f2a4ebb60ed16
Dantel <dantel.williamson2@gmail.com>
2018-04-25 23:28:15 -0600

    second commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+/* <script type="text/javascript"> */
+
+
+
+var computerChoices = ["a","b","c",...];
+// find an alphabet string that is shorter
+
+var wins= 0;
+var losses= 0;
+
+
+document.onkyup = function(event) {
+    
+    var userGuess = event.key
+
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+
+    //What happens if the two guesses equal each other?
+
+// How will you count the amount of trys?
+}
+// pressing the eneter key is how you can start the game
+// lossing = when the amount of trys in the game is up 
+// variable keeping track of prvious guesses while showing them on the screen  
+// alerts --> "You guess correctly" "you lost this time"
+// What will be dsplayed and updated while playing and there are wins and losses
+// What will be displayed when the user guess and computer guess match 
+// What happens when the user runs out of trys
+// 
+
+
+var html =
+
+"<p>wins: " + wins + "</p>" +
+"<p>losses: " + losses + "</p>" +
+"<p>ties: " + ties + "</p>";
+// display the data collected by preivous guesses
+
+
+
+
+
+
